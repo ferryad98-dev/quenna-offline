f98fe65e4469f33198eecf653792441b41feb71e
ferryad98-dev <ferryad98@gmail.com>
2026-08-03 11:23:26 +0700

    v1.6.4 ikon transparan

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    while offline). Setelah aplikasi dibuka sekali, tetap bisa
    dipakai saat internet mati (data terakhir di cache).
    ============================================================ */
-const CACHE = 'pisang-madu-queena-v34';
+const CACHE = 'pisang-madu-queena-v35';
 
 const ASSETS = [
   './',
